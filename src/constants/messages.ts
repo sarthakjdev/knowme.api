0b20b4c6aec06eed9265aeac452ea2dae99df035
sarthakjdev <jsarthak448@gmail.com>
2022-04-22 00:26:07 +0530

    feat: edited ready message

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,10 +1,13 @@
 const messages = {
     serverError: 'Server Error occured',
     success: 'Successfully executed',
-    ready: 'Sarthak\'s API is working fine!',
-    shortAbout: {
+    ready: {
         name: 'Sarthak Jain',
+        email: 'contact.sarthakjain@gmail.com',
+        about: 'Hey everyone, I am Sarthak. I am from Delhi.I am a freelance full-stack developer and discord bot developer,I majorly work with react ecosystem & nodejs at the backend and looking forward to exploring more in the future. ',
         twitter: 'https://twitter.com/sarthakjdev',
+        github: 'https://github.com/sarthakjdev',
+        linkedin: 'https://www.linkedin.com/in/sarthakjdev',
     },
     basic: 'Alpha to developer !! Basic route responding from the server Roger !!',
 }
